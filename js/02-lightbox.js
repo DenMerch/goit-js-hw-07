8c0a95646bb34796e76e15addcc1cbff8e7a53cb
DenMerch <rosignolli@gmail.com>
2023-01-08 17:21:02 +0200

    add node

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallareEl = document.querySelector(".gallery");
+function markup(items) {
+    return items.map(item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+
+};
+const createdGalary = markup(galleryItems);
+gallareEl.insertAdjacentHTML("beforeend", createdGalary);
